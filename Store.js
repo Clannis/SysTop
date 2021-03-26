7a9cb330358830ec7530cef10ee591faeeaae7bd
clannis <dodds1990@gmail.com>
2021-03-26 09:47:01 -0500

    Create Store module to locally store user settings and initialize a store with default settings

---
+++ Store.js
@@ -0,0 +1,31 @@
+const electron = require('electron')
+const path = require('path')
+const fs = reqire ('fs')
+
+class Store {
+    constructor(options) {
+        const userDataPath = (electron.app || electron.remote.app).getPath('userData')
+
+        this.path = path.join(userDataPath, options.configName + '.json')
+        this.data = parseDataFile(this.path, options.defaults)
+    }
+
+    get(key) {
+        return this.data[key]
+    }
+
+    set(key, value) {
+        this.data[key] = value
+        fs.writeFileSync(this.path, JSON.stringify(this.data))
+    }
+}
+
+function parseDataFile(filePath, defaults) {
+    try {
+        return JSON.stringify(fs.readFileSync(filePath))
+    } catch (err) {
+        return defaults
+    }
+}
+
+module.exports = Store
