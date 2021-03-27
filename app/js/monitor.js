0dfc50a02dfa468fa4a9c8d749d7c4d804d95704
clannis <dodds1990@gmail.com>
2021-03-27 17:39:07 -0500

    Correct displayed system info

--- app/js/monitor.js
+++ app/js/monitor.js
@@ -89,14 +89,17 @@ setInterval(() => {
 document.getElementById('cpu-model').innerText = cpu.model()
 
 // Computer Name
-document.getElementById('comp-name').innerText = os.hostname()
+document.getElementById('comp-name').innerText = os.hostname().split('.')[0]
 
 // Set OS
-document.getElementById('os').innerText = `${os.type() === 'Darwin' ? 'MacOS' : os.type() === 'Win' ? 'Windows' : 'Linux'} ${os.arch()}`
+os.oos().then((info) => {
+    document.getElementById('os').innerText = `${info}`
+})
+// document.getElementById('os').innerText = `${os.type() === 'Darwin' ? 'MacOS' : os.type() === 'Win' ? 'Windows' : 'Linux'} ${os.arch()}`
 
 // Set Mem
 mem.info().then(info => {
-    document.getElementById('mem-total').innerText = info.totalMemMb
+    document.getElementById('mem-total').innerText = pretty(bytes(`${info.totalMemMb}mb`))
 })
 
 // Show DHMS
